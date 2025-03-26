aabbb027a3433130273e1d1e632b99d89ce82572
Alex Alex Desatoff <alexdesatoff2007@gmail.com>
2025-03-25 23:30:11 -0300

    Implementing L_LiskovSubstituionPrinciple

---
+++ SOLID/3_l_lsp/lsp_example.py
@@ -0,0 +1,29 @@
+class Animal:
+    def comer(self):
+        print('O animal está comendo!')
+
+    def andar(self):
+        print('O animal está andando na jaula!')
+
+class Felino(Animal):
+    def lamber(self):
+        print('O felino está lambendo seu pelo!')
+
+class Leao(Felino):
+    def rugir(self):
+        print('O leão está rugindo!')
+
+class Pessoa:
+    def observa(self, animal: Animal):
+        animal.comer()
+
+renatinho = Pessoa()
+animal = Animal()
+felino = Felino()
+leao = Leao()
+
+renatinho.observa(animal)
+renatinho.observa(felino)
+renatinho.observa(leao)
+
+# Herda as classes mais genéricas para não ter que repetir funções em classes mais específicas.
